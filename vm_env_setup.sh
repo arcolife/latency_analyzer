713f32dbcaa5875e8b76f734d342a70ee6439341
Archit Sharma <arcsharm@redhat.com>
2016-01-25 19:41:22 +0530

    add commands to install fedora22 image

--- vm_env_setup.sh
+++ vm_env_setup.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-PROJECT_ROOT='/src'
-QCOW_FILE_PATH='/var/lib/libvirt/images/vm1.qcow2'
-QCOW_BLOCK_PATH=${PROJECT_ROOT%/}/vm1.disk1.qcow2
-XML_PATH="${PROJECT_ROOT%/}/vm1.xml"
+vm=vm2
+PROJECT_ROOT="/src"
+IMAGE_PATH="/var/lib/libvirt/images/$vm.qcow2"
+DISK_PATH="/var/lib/libvirt/images/$vm.disk.qcow2"
+# DISK_PATH=${PROJECT_ROOT%/}/$vm.disk1.qcow2
+XML_PATH="${PROJECT_ROOT%/}/$vm.xml"
+ISO_NAME="Fedora-Server-DVD-x86_64-22.iso"
+ISO_LOC="https://dl.fedoraproject.org/pub/fedora/linux/releases/22/Server/x86_64/os/"
 
 # define client name here, ex: virbr0-xxx-xx
-CLIENTS=''
+CLIENTS=""
 
 echo `ps -aef | grep qemu-system-x86`
 
 cleanup(){
 	echo "cleaning up; removing ${PROJECT_ROOT%/}/<related files>.."
-	rm -rf ${PROJECT_ROOT%/}/{kvm_io/,vm1.disk1.qcow2,vm1_snapshot,vm1.xml}
+	rm -rf ${PROJECT_ROOT%/}/{kvm_io/,$vm.disk1.qcow2,$vm_snapshot,$vm.xml}
 }
 
 user_interrupt(){
@@ -28,10 +32,10 @@ trap user_interrupt SIGTSTP
 install_requirements(){
 	cleanup
 	echo -e "\e[1;33m Initiating requirements installation..\e[0m"
-	# create project location and download vm1.xml 
+	# create project location and download $vm.xml 
 	mkdir $PROJECT_ROOT
 	dnf install -q -y wget
-	wget -q https://raw.githubusercontent.com/arcolife/latency_analyzer/master/vm1.xml -O $XML_PATH
+	wget -q https://raw.githubusercontent.com/arcolife/latency_analyzer/master/$vm.xml -O $XML_PATH
 	if [ -f $XML_PATH ]; then
 		echo -e "\e[1;42m $PROJECT_ROOT was created; VM's XML definition saved to $XML_PATH.. \e[0m"
 	else
@@ -82,6 +86,7 @@ install_requirements(){
 	# setup git and clone https://github.com/psuriset/kvm_io.git
 	dnf install -q -y git 
 	git clone -q https://github.com/psuriset/kvm_io.git ${PROJECT_ROOT%/}/kvm_io
+
 	if [ -f ${PROJECT_ROOT%/}/kvm_io/bench_iter.sh ]; then
 		echo -e "\e[1;42m ${PROJECT_ROOT%/}/kvm_io was created.. \e[0m"
 	else
@@ -106,30 +111,58 @@ install_requirements(){
 }
 
 bootstrap_it(){
-	qemu-img create -q -f qcow2 $QCOW_BLOCK_PATH 2G	
-	qemu-img create -q -f qcow2 $QCOW_FILE_PATH 2G	
-	virsh -q define $XML_PATH
-	virsh -q start vm1
-	if [[ $(virsh list | grep vm1) ]]; then
+	dist=fedora22
+	# cd ${PROJECT_ROOT%/}/ && wget $ISO_LOC
+	# get the kickstart file
+	wget -q https://raw.githubusercontent.com/arcolife/latency_analyzer/master/$dist-vm.ks -O ${PROJECT_ROOT%/}/$dist-vm.ks
+
+	# virsh destroy $vm
+	# virsh undefine $vm
+
+	qemu-img create -q -f qcow2 $IMAGE_PATH 10G
+	virt-install --name=$vm \
+		--virt-type=kvm \
+		--disk format=qcow2,path=$IMAGE_PATH \
+		--vcpus=2 \
+		--ram=1024 \
+		--network bridge=virbr0 \
+		--os-type=linux \
+		--os-variant=$dist \
+		--graphics none \
+		--extra-args="ks=file:/$dist-vm.ks console=ttyS0,115200" \
+		--initrd-inject=/src/$dist-vm.ks \
+		--serial pty \
+		--location=$ISO_LOC \
+		--noreboot
+		# --cdrom=${PROJECT_ROOT%/}/$ISO_NAME\
+
+	# dnf install libguestfs-tools-c
+	# virt-builder fedora-23 -o /var/lib/libvirt/images/$vm.qcow2 --format qcow2 --update --selinux-relabel --size 5G
+	         
+	qemu-img create -q -f qcow2 $DISK_PATH 500M
+	# virsh -q define $XML_PATH
+	virsh -q start $vm	
+	virsh attach-device $vm ${PROJECT_ROOT%/}/disk-native.xml --persistent
+	if [[ $(virsh list | grep $vm) ]]; then
 		PID=`pgrep qemu-system-x86 | tail -n 1`
 		echo $PID
-		virsh -q save vm1 ${PROJECT_ROOT%/}/vm1_snapshot
-		echo -e "\e[1;42m vm1 was running with PID $PID ..snapshot saved to $PROJECT_ROOT \e[0m"
+		# virsh -q save $vm ${PROJECT_ROOT%/}/$vm_snapshot
+		echo -e "\e[1;42m $vm was running with PID $PID ..snapshot saved to $PROJECT_ROOT \e[0m"
 	else
-		echo -e "\e[1;31m FAILED! vm1 couldn't start up. \e[0m"
+		echo -e "\e[1;31m FAILED! $vm couldn't start up. \e[0m"
 		exit 1
 	fi
-}
-
-attach_disk(){
-	virsh restore ${PROJECT_ROOT%/}/vm1_snapshot
-	# use kvm_io
-	echo
+	virsh shutdown $vm
 }
 
 run_workload(){
 	# run kvm_io/bench_iter.sh
-	echo
+	virsh start $vm
+	# virsh restore ${PROJECT_ROOT%/}/$vm_snapshot
+	# qemu-img info master.qcow2
+	cd ${PROJECT_ROOT%/}/kvm_io/
+	chmod +x bench_iter.sh
+	./bench_iter.sh
 }
 
 process_data(){
@@ -139,6 +172,5 @@ process_data(){
 
 install_requirements
 bootstrap_it
-# attach_disk
 # run_workload
 # process_data
